30acec3a4464be3889975ea5f96970cf4bb6f739
Krisna Satriadi <krisna.s.362@gmail.com>
2021-12-17 10:06:35 +0700

    change index.php background and mam

--- index.php
+++ index.php
@@ -15,16 +15,17 @@
   <title>Howler</title>
 </head>
 
-<body>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Howler</a>
+<body class="body">
+  <!-- NAVBAR -->
+  <nav class="navbar navbar-expand-sm navbar-dark">
+    <a class="navbar-brand text-sm" href="#">Howler</a>
     <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavId">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link shadow-text" href="./index.php">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
@@ -41,15 +42,19 @@
         <form class="form-inline my-2 my-lg-0 mr-0 col-12">
           <input class="form-control mr-sm-2" type="text" placeholder="Username">
           <input class="form-control mr-sm-2" type="text" placeholder="Password">
-          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Login</button>
+          <button class="btn btn-outline-light my-2 my-sm-0 shadow-text" type="submit">Login</button>
         </form>
         <div class="col-12 text-light text-center">
           Don't have an account?
-          <a href="./register.php">Sign up</a>
+          <a href="./register.php" class="text-light shadow-text">Sign up</a>
         </div>
       </div>
     </div>
   </nav>
+  <section>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  </section>
+  <!-- END OF NAVBAR -->
   <!-- Optional JavaScript; choose one of the two! -->
   <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
