8ed556da8fc28330c2a410da7f852c9d689e8d37
Krisna Satriadi <krisna.s.362@gmail.com>
2021-12-22 11:34:19 +0700

    edit connection

--- config.php
+++ config.php
@@ -1,10 +1,5 @@
 <?php
-define("DB_SERVER", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "social_media_test");
-
-$connection = mysqli_connect("DB_SERVER", "DB_USERNAME", "DB_PASSWORD", "DB_NAME");
+$connection = mysqli_connect("localhost", "root", "", "social_media_test");
 
 if(mysqli_connect_errno()) {
   echo `Failed to connect database` . mysqli_connect_error();
