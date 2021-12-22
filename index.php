df7685cb4eb8388dc184ccb123461f151d883847
Krisna Satriadi <krisna.s.362@gmail.com>
2021-12-22 09:15:28 +0700

    change knotype to kontext

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark private-style-navbar">
     <a class="navbar-brand " href="./index.php">
       <img src="./assets/images/ichun.png" alt="logo">
-      Knotype
+      Knotext
     </a>
     <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
